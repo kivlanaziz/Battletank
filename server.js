5b6b0329f11aa6d81d78db64f1aec2afeb2685b2
Naufal <nmuntaaza.wh@gmail.com>
2019-10-24 01:05:37 +0700

    revert

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.use('/public/assets', express.static('./public/assets'));
-app.use('/public/css', express.static('./public/css'));
-app.use('/public/js', express.static('./public/js'));
-app.use('/public/lib', express.static('./public/lib'));
+app.use(express.static('./public'));
 
 const PORT = 8080;
 const HOSTNAME = 'localhost';
 
-app.get('/sample', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/game.html'));
 });
 
 app.listen(PORT, HOSTNAME, () => {
